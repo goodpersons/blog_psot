6927860449bf32f6ae18a87959eb937169f53014
张贵林 <zhangguilin@baklib.vip>
2023-08-03 15:37:56 +0800

    add published_at draft published scheduled blogpost

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,22 @@
 class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  scope :sorted, -> { order(published_at: :asc, updated_at: :desc) }
+  scope :draft, -> { where(published_at: nil) }  #BlogPost.draft  查询所有草稿状态的博客
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :scheduled, -> { where("published_at > ?", Time.current) }
+
+  # 模型辅助方法
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at >= Time.current
+  end
 end
